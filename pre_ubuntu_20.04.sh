adf3f7a1d78e5ce15633f55d62fde43ef621b909
leone <791960492@qq.com>
2021-01-08 19:34:13 +0800

    add pre.sh

---
+++ pre_ubuntu_20.04.sh
@@ -0,0 +1,12 @@
+make preparation
+sudo cp external/toolset/ubuntu20.04/{as,ld,ld.gold,objdump} /usr/local/bin
+sudo apt-get install build-essential ocaml ocamlbuild automake autoconf libtool wget python-is-python3 libssl-dev git cmake perl -y
+sudo apt-get install libssl-dev libcurl4-openssl-dev protobuf-compiler libprotobuf-dev debhelper cmake reprepro unzip -y
+sudo apt-get install build-essential python -y
+sudo apt-get install libssl-dev libcurl4-openssl-dev libprotobuf-dev -y
+if [ $(grep -Ec "^[^#].*sgx_debian_local_repo" /etc/apt/sources.list) -eq 0 ]; then
+  echo "add apt source"
+  sudo sh -c "echo \"deb [trusted=yes arch=amd64] file:/home/leone/文档/linux-sgx/linux/installer/deb/sgx_debian_local_repo focal main\" >>/etc/apt/sources.list"
+else
+  echo "already added apt source"
+fi
